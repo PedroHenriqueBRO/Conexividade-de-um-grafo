29aaf8a86978ca1b49a0d949269017982419ba41
Pedro <xtron44@gmail.com>
2025-06-02 23:09:14 -0300

    Algoritimo de ponte feito para verificar a conexividade de um grafo com ins e outs para teste

--- grafo.cpp
+++ grafo.cpp
@@ -3,109 +3,132 @@
 #include <string>
 #include <vector>
 using namespace std;
-
-void DFS(int pos, vector<vector<int>> &G, vector<string> &visitado) {
-  stack<int> pilha;
-  pilha.push(pos);
-  while (pilha.size() != 0) {
-    int v = pilha.top();
-    pilha.pop();
-    if (visitado[v] == "Branco") {
-      visitado[v] = "Cinza";
-      for (const auto &adj : G[v]) {
-        if (visitado[adj] == "Branco") {
-          pilha.push(adj);
-        }
-      }
-      visitado[v] = "Preto";
-    }
-    if (visitado[v] == "Negado") {
-      for (const auto &adj : G[v]) {
-        if (visitado[adj] == "Branco") {
-          pilha.push(adj);
-          break;
-        }
-      }
-    }
-  }
-}
-bool conexo(vector<vector<int>> &G, int pos, vector<string> visitado) {
-  DFS(pos, G, visitado);
-  for (int i = 0; i < visitado.size(); i++) {
-    if (visitado[i] == "Negado") {
-      continue;
-    }
-    if (visitado[i] != "Preto") {
-      return false;
-    }
-  }
-  return true;
-}
-bool conexividade1(vector<vector<int>> &G) {
-  vector<int> vqd;
-  bool chave = false;
-  for (int i = 0; i < G.size(); i++) {
-    vector<string> visitado(G.size(), "Branco");
-    visitado[i] = "Negado";
-    bool c = conexo(G, i, visitado);
-    if (c == false) {
-      vqd.push_back(i);
-      chave = true;
-    }
-  }
-  if (chave) {
-    cout << "Grafo 1-conexo." << endl;
-  }
-  for (const auto &v : vqd) {
-    cout << v << endl;
-  }
-  return chave;
+//0=branco,1=cinza,2=preto
+void remove(vector<vector<int>>&G,int pos){ 
+	G[pos].clear(); 
+	for(int i=0;i<G.size();i++){ 
+		for(int j=0;j<G[i].size();j++){ 
+			if(G[i][j]==pos){ 
+				G[i].erase(G[i].begin()+j); 
+			} 
+		} 
+	} 
 }
 
-bool conexividade2(vector<vector<int>> &G) {
-  vector<pair<int, int>> pares;
-  bool chave = false;
-  for (int i = 0; i < G.size(); i++) {
-    vector<string> visitado(G.size(), "Branco");
-    visitado[i] = "Negado";
-    bool c = false;
-    for (int j = i + 1; j < G.size(); j++) {
-      visitado[j] = "Negado";
-      c = conexo(G, i, visitado);
-      if (c == false) {
-        pair<int, int> par;
-        par.first = i;
-        par.second = j;
-        pares.push_back(par);
-      }
-      visitado[j] = "Branco";
-    }
-  }
-  if (pares.size() != 0) {
-    cout << "Grafo 2-conexo." << endl;
-  } else {
-    return false;
-  }
-  for (const auto &vs : pares) {
-    cout << vs.first << " " << vs.second << endl;
-  }
-  return true;
+int DFS(int &tempo,int atual,const vector<vector<int>> &G, vector<int> &visitado,vector<pair<int,int>>&inifim,vector<bool>&verticescorte) {
+	visitado[atual]=1;
+	inifim[atual].first=tempo++;
+	int melhor=tempo;
+	for(int i=0;i<G[atual].size();i++){
+		int v=G[atual][i];
+		int cand;
+		if(visitado[v]==0){
+			cand = DFS(tempo,v,G,visitado,inifim,verticescorte);
+			if(cand>=inifim[atual].first){
+				verticescorte[atual]=true;
+			}	
+			melhor=min(cand,melhor);
+		}else{
+			melhor =min(inifim[v].first,melhor);
+		}
+
+	}
+	inifim[atual].second=tempo++;
+	visitado[atual]=2;	
+	return melhor;
+
 }
+void _DFS(int &tempo,int atual,const vector<vector<int>> &G, vector<int> &visitado,vector<pair<int,int>>&inifim,vector<bool>&verticescorte) {
+	visitado[atual]=1;
+	inifim[atual].first=tempo++;
+	bool primeiro=true;
+	for(int i=0;i<G[atual].size();i++){
+		int v=G[atual][i];
+		if(visitado[v]==0){
+			if(!primeiro){
+				verticescorte[atual]=true;
+			}
+			DFS(tempo,v,G,visitado,inifim,verticescorte);
+			primeiro=false;
+		}
+	}
+	inifim[atual].second=tempo++;
+	visitado[atual]=2;
+}
+void algoritmoDePonte2(vector<vector<int>>&G){
+	int chave3=0;
+	vector<vector<pair<int, int>>> pares(G.size(), vector<pair<int, int>>(G.size(), {-1, -1}));
+	for(int i=0;i<G.size();i++){
+
+		vector<int>visitado(G.size(),0);
+		vector<pair<int,int>>inifim(G.size(),{-1,-1});
+		vector<bool>verticescorte(G.size(),false);
+		vector<vector<int>>aux=G;
+		int tempo=0;
+		remove(aux,i);
+		int pos=i+1;
+		if(i+1==G.size()){
+			pos=0;
+		}
+		_DFS(tempo,pos,aux,visitado,inifim,verticescorte);
+		for(int j=0;j<verticescorte.size();j++){
+			if(verticescorte[j]==true){
+				chave3++;
+				if(chave3==1){
+					cout<<"Grafo 2-conexo."<<endl;
+				}
+				if(pares[j][i].first==-1){
+				pares[i][j].first=i;
+				pares[i][j].second=j;
+				}
+			}
+	}
+}
+for(const auto& p:pares){
+	for(int i=0;i<p.size();i++){
+		if(p[i].first!=-1){
+			cout<<p[i].first<<" "<<p[i].second<<endl;
+		}
+	}
+}
+if(chave3==0){
+	cout<<"Grafo 3-conexo."<<endl;
+}
+}
+void algoritmoDePonte(vector<vector<int>> &G) {
+	vector<int>visitado(G.size(),0);
+	vector<pair<int,int>>inifim(G.size(),{-1,-1});
+	vector<bool>verticescorte(G.size(),false);
+	int tempo=0;
+	_DFS(tempo,0,G,visitado,inifim,verticescorte);
+	int chave=0;
+	for(int i=0;i<verticescorte.size();i++){
+		if(verticescorte[i]==true){
+			chave++;
+			if(chave==1){
+				cout<<"Grafo 1-conexo."<<endl;
+			}	
+			cout<<i<<endl;
+		}
+	}
+	if(chave==0){
+		algoritmoDePonte2(G);
+	}
+}
+
+
 int main() {
-  int n, m;
-  cin >> n >> m;
-  vector<vector<int>> G(n);
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> G(n);
 
-  for (int i = 0; i < m; i++) {
-    int are1, are2;
-    cin >> are1 >> are2;
-    G[are1].push_back(are2);
-    G[are2].push_back(are1);
-  }
-  if (!conexividade1(G)) {
-    if (!conexividade2(G)) {
-      cout << "Grafo 3-conexo." << endl;
-    }
-  }
-  return 0;
+	for (int i = 0; i < m; i++) {
+		int are1, are2;
+		cin >> are1 >> are2;
+		G[are1].push_back(are2);
+		G[are2].push_back(are1);
+	}
+	algoritmoDePonte(G);
+
+	return 0;
 }
